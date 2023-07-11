19bdc464097aa2feb649629f69e9b5cabd3f9e58
yunigongshang <2870454770@qq.com>
2023-07-11 15:26:47 +0800

    modified:   node/sever.js

--- node/sever.js
+++ node/sever.js
@@ -1,9 +1,8 @@
 const   userRouter = require('./dam.js')
 const express = require('express')
 const app = express()
-// app.use(userRouter)
+app.use(userRouter)
 app.use('/api',userRouter)
-
 app.listen(8083,()=>{
-    console.log('http://localhost:8083')
+    console.log('http://localhost:8083/user/list')
 })

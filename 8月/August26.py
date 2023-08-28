f180f2e9e9717d4763f06cf5c84fc529f7fd84ae
yunigongshang <2870454770@qq.com>
2023-08-28 08:47:48 +0800

    modified:   "8\346\234\210/August26.py" 	new file:   ke.json 	modified:   post.py 	deleted:    "\345\205\266\344\273\226/ke.json" 	deleted:    "\350\257\276\350\241\250.xlsx"

--- 8月/August26.py
+++ 8月/August26.py
@@ -15,6 +15,7 @@ def jiejc(a):
         for i in range(find_numbers[0],find_numbers[1]+1):
             b.append(i)
         return b
+    
 try:
     res=requests.post(
         url="http://111.75.254.215:9002/jwglxt/kbcx/xskbcx_cxXsgrkb.html?gnmkdm=N2151&su=2021132697",
@@ -24,7 +25,7 @@ try:
             "Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
             "Content-Length":"28",
             "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8",
-            "Cookie":"JSESSIONID=6D4F0D2C64C228DB71DDCCEA777C3917",
+            "Cookie":"JSESSIONID=8D93FF3879E5CBF93C4AB396BEC70506",
             "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.54"
         },
         data={
@@ -58,13 +59,13 @@ for i in range(len(r)):
     #要写入的数据
     data=f'课名：{r[i]["kcmc"]},老师：{r[i]["xm"]},地点：{r[i]["cdmc"]}'
     for xqjmc in jiejc(r[i]["zcd"]): #周次
-        worksheet=workbook.worksheets[int(xqjmc)-1]#选择工作表
+        worksheet=workbook.worksheets[xqjmc-1]#选择工作表
         for jc in jiejc(r[i]["jc"]): #课节
             cell = worksheet[f'{tmokey[r[i]["xqjmc"]]}{jc+1}']#选择单元格
             cell.value = data  #进行写入
             #设置宽高
             column_letter = get_column_letter(cell.column)
             worksheet.column_dimensions[column_letter].width = 65
-    
-workbook.save('课表.xlsx')
+name=res["xsxx"]
+workbook.save(f'{name["BJMC"]}{name["XM"]}.xlsx')
 print("导出成功")
---
+++ post.py
@@ -0,0 +1,3 @@
+import json
+with open('ke.json', 'r',encoding='utf-8') as file:
+   res = json.load(file)
